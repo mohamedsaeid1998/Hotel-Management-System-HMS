f19374fe7c8bcb9cc103b3812ce821dc0d441046
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2024-01-12 20:30:07 +0200

    enhance getFacilitiesData function
    enhance getFacilitiesData function  pass it within useCallback

--- src/Pages/Facilities/Facilities.tsx
+++ src/Pages/Facilities/Facilities.tsx
@@ -1,122 +1,133 @@
-import { TableHeader } from '@/Components';
-import { FacilitiesData } from '@/Redux/Features/Facilities/FacilitiesSlice';
-import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid';
-import moment from 'moment';
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import './Facilities.module.scss';
+/** @format */
+
+import { TableHeader } from "@/Components";
+import { FacilitiesData } from "@/Redux/Features/Facilities/FacilitiesSlice";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import moment from "moment";
+import { useCallback, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import "./Facilities.module.scss";
+import "../../Styles/global.scss";
 
 const Facilities = () => {
-
   const dispatch = useDispatch();
-  const [tableData, setTableData] = useState([])
+  const [tableData, setTableData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const getFacilitiesData = async () => {
-    // @ts-ignore
-    let element = await dispatch(FacilitiesData())
-    // @ts-ignore
-    setTableData(element.payload.data.facilities)
-  }
-console.log(tableData);
+  const getFacilitiesData = useCallback(async () => {
+    setLoading(true);
+    try {
+      // @ts-ignore
+      const element = await dispatch(FacilitiesData());
+      // @ts-ignore
+      setTableData(element.payload.data.facilities);
+    } finally {
+      setLoading(false);
+    }
+  }, [dispatch]);
 
   useEffect(() => {
-    getFacilitiesData()
+    getFacilitiesData();
   }, []);
 
-
-const tableBody: GridColDef[] = [
-  {
-    field: 'name',
-    headerName: 'facility Name',
-    width: 290,
-    editable: false,
-    
-  },
-  // {
-  //   field: 'createdBy',
-  //   headerName: 'createdBy',
-  //   width: 290,
-  //   editable: false,
-  //   renderCell: (params) => {
-  //     return (console.log(params.row.createdBy))
-  //   },
-  // },
-  {
-    field: 'createdAt',
-    headerName: 'Created At',
-    width: 290,
-    editable: false,
-    renderCell: (params) => {
-      return <span>{moment(params?.formattedValue).format("Do MMM YY")}</span>
-    }
-  },
-
-  {
-    field: 'updatedAt',
-    headerName: 'Updated At',
-    width: 290,
-    editable: false,
-    renderCell: (params) => {
-      return <span>{moment(params?.formattedValue).format("Do MMM YY")}</span>
-    }
-  },
-
-  {
-    field: "action",
-    headerName: "Action",
-    width: 290,
-    renderCell: (params) => {
-      // const { id, name } = params.row;
-
-
-      return (<>
-        <div className="action d-flex align-items-center gap-3 ">
-          <div className="edit text-info pointer">
-here
-          </div>
-          <div className="delete text-danger pointer" >
-there          </div>
-        </div>
-
-      </>
-      );
+  const tableBody: GridColDef[] = [
+    {
+      field: "name",
+      headerName: "facility Name",
+      width: 290,
+      editable: false,
     },
-  },
-];
-
-  return <>
-<TableHeader title={"Facilities"} subTitle={"facility"} path={'/dashboard/add-new-facility'}/>
-
-
-
-<DataGrid
-    className='dataGrid'
-    rows={tableData}
-    columns={tableBody}
-    getRowId={(row) => row._id}
-    initialState={{
-      pagination: {
-        paginationModel: {
-          pageSize:5,
-        },
+    // {
+    //   field: 'createdBy',
+    //   headerName: 'createdBy',
+    //   width: 290,
+    //   editable: false,
+    //   renderCell: (params) => {
+    //     return (console.log(params.row.createdBy))
+    //   },
+    // },
+    {
+      field: "createdAt",
+      headerName: "Created At",
+      width: 290,
+      editable: false,
+      renderCell: (params) => {
+        return (
+          <span>{moment(params?.formattedValue).format("Do MMM YY")}</span>
+        );
       },
-    }}
-    slots={{ toolbar: GridToolbar }}
-    slotProps={{
-      toolbar: {
-        showQuickFilter: true,
-        quickFilterProps: { debounceMs: 500 },
-      }
-    }}
-    pageSizeOptions={[5,10]}
-    checkboxSelection
-    // disableRowSelectionOnClick
-    // disableColumnFilter
-    // disableDensitySelector
-    // disableColumnSelector
-  />
+    },
 
-  </>
-}
+    {
+      field: "updatedAt",
+      headerName: "Updated At",
+      width: 290,
+      editable: false,
+      renderCell: (params) => {
+        return (
+          <span>{moment(params?.formattedValue).format("Do MMM YY")}</span>
+        );
+      },
+    },
 
-export default Facilities
+    {
+      field: "action",
+      headerName: "Action",
+      width: 290,
+      renderCell: (params) => {
+        // const { id, name } = params.row;
+
+        return (
+          <>
+            <div className="action d-flex align-items-center gap-3 ">
+              <div className="edit text-info pointer">here</div>
+              <div className="delete text-danger pointer">there </div>
+            </div>
+          </>
+        );
+      },
+    },
+  ];
+
+  return (
+    <>
+      <TableHeader
+        title={"Facilities"}
+        subTitle={"facility"}
+        path={"/dashboard/add-new-facility"}
+      />
+
+      <DataGrid
+        className="dataGrid tableStyle"
+        rows={tableData}
+        columns={tableBody}
+        getRowId={(row) => row._id}
+        rowSelectionModel={"server"}
+        rowCount={2}
+        loading={loading}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        slots={{ toolbar: GridToolbar }}
+        slotProps={{
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+        // disableRowSelectionOnClick
+        // disableColumnFilter
+        // disableDensitySelector
+        // disableColumnSelector
+      />
+    </>
+  );
+};
+
+export default Facilities;
