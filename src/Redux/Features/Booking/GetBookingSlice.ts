1be63ff90f37a65de5658bc307e0b989b7d8264c
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2024-01-14 04:44:51 +0200

    booking slice

--- src/Redux/Features/Booking/GetBookingSlice.ts
+++ src/Redux/Features/Booking/GetBookingSlice.ts
@@ -1,53 +1,56 @@
+/** @format */
+
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import baseUrl from "../../../utils/Custom/Custom";
 
-
 export interface Props {
-  data: any[]
-  loading:boolean
-  error:null|string
+  data: any[];
+  loading: boolean;
+  error: null | string;
 }
-const token = localStorage.getItem("authToken")
+const token = localStorage.getItem("authToken");
 
-export const BookingData = createAsyncThunk<any, void>("GetBookingSlice/BookingData", async () => {
-  let data = await baseUrl.get(`/api/v0/admin/booking?page=1&size=100`,{
-    headers:{
-      Authorization: token
-    }
+export const BookingData = createAsyncThunk<any, void>(
+  "GetBookingSlice/BookingData",
+  async () => {
+    let data = await baseUrl.get(`/api/v0/admin/booking?page=1&size=100`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    return data.data;
+  }
+);
 
-  })
-  return data.data
-})
-
-
-
-
-
-let initialState:Props = {
+let initialState: Props = {
   data: [],
   loading: false,
-  error: null
-}
-
+  error: null,
+};
 
 export const GetBookingSlice = createSlice({
-  name: 'BookingData',
+  name: "BookingData",
   initialState,
-  reducers:{},
+  reducers: {},
   extraReducers: (builder) => {
     builder.addCase(BookingData.pending, (state) => {
       state.loading = true;
     });
-    builder.addCase(BookingData.fulfilled, (state, action: PayloadAction<any>) => {
-      state.loading = false;
-      state.data = action.payload;
-    });
-    builder.addCase(BookingData.rejected, (state, action: PayloadAction<any>) => {
-      state.loading = false;
-      state.error = action.payload.message;
-    });
-  }
+    builder.addCase(
+      BookingData.fulfilled,
+      (state, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.data = action.payload;
+      }
+    );
+    builder.addCase(
+      BookingData.rejected,
+      (state, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      }
+    );
+  },
+});
 
-})
-
-export default GetBookingSlice.reducer
+export default GetBookingSlice.reducer;
