b42274366c90dbf251de3744e2e54867e14a6247
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2024-01-16 01:48:14 +0200

    edit room

--- src/Pages/AddNewRoom/AddNewRoom.tsx
+++ src/Pages/AddNewRoom/AddNewRoom.tsx
@@ -22,7 +22,6 @@ const AddNewRoom = () => {
   const [checkPage, setCheckPage] = useState(false);
   const [roomDetails, setRoomDetails] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [test, setTest] = useState("");
   const { id } = useParams();
   const location = useLocation();
   const { isEdit } = location.state as propState;
@@ -31,6 +30,7 @@ const AddNewRoom = () => {
     handleSubmit,
     getValues,
     setValue,
+    trigger,
 
     formState: { errors },
   } = useForm();
@@ -79,16 +79,15 @@ const AddNewRoom = () => {
   useEffect(() => {
     getFacilitiesData();
     if (isEdit) {
-      setRoomId(id);
       const getRoomDetails = async () => {
         setLoading(true);
         try {
-          const getEditRoomData = await dispatch(RoomsDataDetails(roomId));
-          setRoomDetails(getEditRoomData.payload.data.room);
-          setValue("price", roomDetails?.price);
-          setValue("roomNumber", roomDetails?.roomNumber);
-          setValue("discount", roomDetails?.discount);
-          setValue("capacity", roomDetails?.capacity);
+          const getEditRoomData = await dispatch(RoomsDataDetails(id));
+          const rdata = getEditRoomData.payload.data.room;
+          setValue("price", rdata?.price);
+          setValue("roomNumber", rdata?.roomNumber);
+          setValue("discount", rdata?.discount);
+          setValue("capacity", rdata?.capacity);
         } catch (error) {
           toast.error(error);
         } finally {
@@ -98,7 +97,7 @@ const AddNewRoom = () => {
       getRoomDetails();
       setCheckPage(isEdit);
     }
-  }, []);
+  }, [id]);
 
   //! ***************Selected Input ***************
 
@@ -153,135 +152,139 @@ const AddNewRoom = () => {
 
   return (
     <>
-      <Box
-        className="formContainer"
-        component="form"
-        onSubmit={handleSubmit(sendData)}
-      >
-        <TextField
-          variant="filled"
-          type="text"
-          className="roomNumber"
-          label="Room Number"
-          {...register("roomNumber", {
-            required,
-            minLength: { value: 3, message: "minlength is 3 " },
-            validate: (value) =>
-              (value !== undefined && +value > 0) ||
-              "Please enter a positive number",
-          })}
-          error={Boolean(errors.roomNumber)}
-          helperText={
-            Boolean(errors.roomNumber)
-              ? errors?.roomNumber?.message?.toString()
-              : null
-          }
-        />
-
-        <Box className="middleInputs">
+      {loading ? (
+        "loading"
+      ) : (
+        <Box
+          className="formContainer"
+          component="form"
+          onSubmit={handleSubmit(sendData)}
+        >
           <TextField
             variant="filled"
-            type="number"
-            className="price"
-            label="Price"
-            {...register("price", {
+            type="text"
+            className="roomNumber"
+            label="Room Number"
+            {...register("roomNumber", {
               required,
+              minLength: { value: 3, message: "minlength is 3 " },
               validate: (value) =>
                 (value !== undefined && +value > 0) ||
                 "Please enter a positive number",
             })}
-            error={!!errors.price}
+            error={Boolean(errors.roomNumber)}
             helperText={
-              !!errors.price ? errors?.price?.message?.toString() : null
-            }
-          />
-
-          <TextField
-            variant="filled"
-            type="number"
-            className="capacity"
-            label="Capacity"
-            {...register("capacity", {
-              required,
-              validate: (value) =>
-                (value !== undefined && +value > 0) ||
-                "Please enter a positive number",
-            })}
-            error={Boolean(errors.capacity)}
-            helperText={
-              Boolean(errors.capacity)
-                ? errors?.capacity?.message?.toString()
-                : null
-            }
-          />
-        </Box>
-
-        <Box className="middleInputs">
-          <TextField
-            variant="filled"
-            type="number"
-            className="discount"
-            label="Discount"
-            {...register("discount", {
-              required,
-              validate: (value) =>
-                (value !== undefined && +value >= 0) ||
-                "Please enter a positive number",
-            })}
-            error={Boolean(errors.discount)}
-            helperText={
-              Boolean(errors.discount)
-                ? errors?.discount?.message?.toString()
+              Boolean(errors.roomNumber)
+                ? errors?.roomNumber?.message?.toString()
                 : null
             }
           />
 
-          <TextField
-            label="select facilities"
-            className="facilities"
-            color="secondary"
-            onClick={() => setFacilities(facilities)}
-            SelectProps={{ multiple: true }}
-            {...register("facilities", {
-              required,
-            })}
-            error={!!errors.facilities}
-            helperText={
-              !!errors.facilities
-                ? errors?.facilities?.message?.toString()
-                : null
-            }
-          >
-            {selectData?.map(({ _id, name }: any) => (
-              <MenuItem key={_id} value={_id}>
-                {name}
-              </MenuItem>
-            ))}
-          </TextField>
-        </Box>
-        <Box className="imagesBtn">
-          <Button
-            component="label"
-            variant="contained"
-            startIcon={<CloudUploadIcon />}
-          >
-            Upload Images
-            <input onChange={handleImageChange} type="file" multiple hidden />
-          </Button>
-        </Box>
+          <Box className="middleInputs">
+            <TextField
+              variant="filled"
+              type="number"
+              className="price"
+              label="Price"
+              {...register("price", {
+                required,
+                validate: (value) =>
+                  (value !== undefined && +value > 0) ||
+                  "Please enter a positive number",
+              })}
+              error={!!errors.price}
+              helperText={
+                !!errors.price ? errors?.price?.message?.toString() : null
+              }
+            />
 
-        <Box className="btnContainer">
-          <Link to={"/dashboard/rooms"}>
-            <Button variant="outlined" size="large">
-              Cancel
+            <TextField
+              variant="filled"
+              type="number"
+              className="capacity"
+              label="Capacity"
+              {...register("capacity", {
+                required,
+                validate: (value) =>
+                  (value !== undefined && +value > 0) ||
+                  "Please enter a positive number",
+              })}
+              error={Boolean(errors.capacity)}
+              helperText={
+                Boolean(errors.capacity)
+                  ? errors?.capacity?.message?.toString()
+                  : null
+              }
+            />
+          </Box>
+
+          <Box className="middleInputs">
+            <TextField
+              variant="filled"
+              type="number"
+              className="discount"
+              label="Discount"
+              {...register("discount", {
+                required,
+                validate: (value) =>
+                  (value !== undefined && +value >= 0) ||
+                  "Please enter a positive number",
+              })}
+              error={Boolean(errors.discount)}
+              helperText={
+                Boolean(errors.discount)
+                  ? errors?.discount?.message?.toString()
+                  : null
+              }
+            />
+
+            <TextField
+              label="select facilities"
+              className="facilities"
+              color="secondary"
+              onClick={() => setFacilities(facilities)}
+              SelectProps={{ multiple: true }}
+              {...register("facilities", {
+                required,
+              })}
+              error={!!errors.facilities}
+              helperText={
+                !!errors.facilities
+                  ? errors?.facilities?.message?.toString()
+                  : null
+              }
+            >
+              {selectData?.map(({ _id, name }: any) => (
+                <MenuItem key={_id} value={_id}>
+                  {name}
+                </MenuItem>
+              ))}
+            </TextField>
+          </Box>
+          <Box className="imagesBtn">
+            <Button
+              component="label"
+              variant="contained"
+              startIcon={<CloudUploadIcon />}
+            >
+              Upload Images
+              <input onChange={handleImageChange} type="file" multiple hidden />
             </Button>
-          </Link>
+          </Box>
 
-          <Button variant="contained" type="submit" size="large">
-            Submit <ChevronRight />
-          </Button>
+          <Box className="btnContainer">
+            <Link to={"/dashboard/rooms"}>
+              <Button variant="outlined" size="large">
+                Cancel
+              </Button>
+            </Link>
+
+            <Button variant="contained" type="submit" size="large">
+              Submit <ChevronRight />
+            </Button>
+          </Box>
         </Box>
-      </Box>
+      )}
     </>
   );
 };
