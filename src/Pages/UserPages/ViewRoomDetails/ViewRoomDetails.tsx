4b3c7f2122fadcb4237de5ab65f2dc864a1efadd
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2024-01-22 12:16:55 +0200

    add MUI calendar

--- src/Pages/UserPages/ViewRoomDetails/ViewRoomDetails.tsx
+++ src/Pages/UserPages/ViewRoomDetails/ViewRoomDetails.tsx
@@ -19,6 +19,7 @@ import {
   CssBaseline,
   List,
   ListItem,
+  ListItemText,
   ThemeProvider,
   Typography,
   createTheme,
@@ -28,6 +29,7 @@ import Grid from "@mui/material/Unstable_Grid2";
 import { styled } from "@mui/material/styles";
 
 import "./ViewRoomStyle.scss";
+import BookingCalender from "@/Components/BookingCalender";
 
 declare module "@mui/material/styles" {
   interface TypographyVariants {
@@ -73,6 +75,12 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: "center",
   color: theme.palette.text.secondary,
 }));
+const Booking = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 const ViewRoomDetails = () => {
   const images = [
     { src: bedImg, caption: `5 bedroom` },
@@ -86,108 +94,112 @@ const ViewRoomDetails = () => {
   ];
   return (
     <>
-      <Container fixed>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Box style={{ margin: "2rem" }}>
-            <Typography variant="poster">Village Angga</Typography>
-            <Typography variant="inherit">Bogor, Indonesia</Typography>
-          </Box>
-          <Box>
-            <BreadcrumbsComponent />
-          </Box>
-          <Box style={{ margin: "2rem" }}>{/* <Breadcrumbs /> */}</Box>
-          <Box
-            className="gridImage"
-            style={{ backgroundColor: "red", textAlign: "center" }}
+      {/* <Container fixed> */}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box style={{ margin: "2rem" }}>
+          <Typography variant="poster">Village Angga</Typography>
+          <Typography variant="inherit">Bogor, Indonesia</Typography>
+        </Box>
+        <Box>
+          <BreadcrumbsComponent />
+        </Box>
+        <Box style={{ margin: "2rem" }}>{/* <Breadcrumbs /> */}</Box>
+        <Box className="gridImage" style={{ textAlign: "center" }}>
+          <div className="gallery__img">
+            <img src={imgView3} alt="" />
+          </div>
+          <div className="gallery__img">
+            <img src={defaultImage} alt="" />
+            <img src={imgView2} alt="" />
+          </div>
+        </Box>
+        <Box>
+          {" "}
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
           >
-            <div className="gallery__img">
-              <img src={imgView3} alt="" />
-            </div>
-            <div className="gallery__img">
-              <img src={defaultImage} alt="" />
-              <img src={imgView2} alt="" />
-            </div>
-          </Box>
-          <Box>
-            {" "}
-            <Grid
-              container
-              rowSpacing={1}
-              columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-            >
-              <Grid xs={6}>
-                <Item>
-                  <List>
-                    <ListItem>
-                      <ListItemText
-                        // primary="  "
-                        sx={{ marginY: 0 }}
-                      >
-                        <Typography>
-                          Minimal techno is a minimalist subgenre of techno
-                          music. It is characterized by a stripped-down
-                          aesthetic that exploits the use of repetition and
-                          understated development. Minimal techno is thought to
-                          have been originally developed in the early 1990s by
-                          Detroit-based producers Robert Hood and Daniel Bell.
-                        </Typography>
-                      </ListItemText>
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText sx={{ marginY: 0 }}>
-                        <Typography>
-                          Such trends saw the demise of the soul-infused techno
-                          that typified the original Detroit sound. Robert Hood
-                          has noted that he and Daniel Bell both realized
-                          something was missing from techno in the post-rave
-                          era.
-                        </Typography>
-                      </ListItemText>
-                    </ListItem>
-                    <ListItem>
-                      <ListItemText sx={{ marginY: 0 }}>
-                        <Typography>
-                          Design is a plan or specification for the construction
-                          of an object or system or for the implementation of an
-                          activity or process, or the result of that plan or
-                          specification in the form of a prototype, product or
-                          process. The national agency for design: enabling
-                          Singapore to use design for economic growth and to
-                          make lives better.
-                        </Typography>
-                      </ListItemText>
-                    </ListItem>
-                  </List>
-                  <Grid container spacing={1}>
-                    {images.slice(0, 4).map((image, index) => (
-                      <Grid item key={index} xs={3}>
-                        <img src={image.src} alt="" />
-                        <Typography>{image.caption}</Typography>
-                      </Grid>
-                    ))}
-                  </Grid>
-                  <Grid container spacing={1} style={{ margin: " 2em 0 " }}>
-                    {images.slice(4, 8).map((image, index) => (
-                      <Grid item key={index} xs={3}>
-                        <img src={image.src} alt="" />
-                        <Typography>{image.caption}</Typography>
-                      </Grid>
-                    ))}
-                  </Grid>
-                </Item>
-              </Grid>
-              <Grid xs={6}>
-                <Grid>2</Grid>
-              </Grid>
-
-              <Grid xs={6}>
-                <Item>4</Item>
+            <Grid xs={6}>
+              <Item>
+                <List>
+                  <ListItem>
+                    <ListItemText
+                      // primary="  "
+                      sx={{ marginY: 0 }}
+                    >
+                      <Typography>
+                        Minimal techno is a minimalist subgenre of techno music.
+                        It is characterized by a stripped-down aesthetic that
+                        exploits the use of repetition and understated
+                        development. Minimal techno is thought to have been
+                        originally developed in the early 1990s by Detroit-based
+                        producers Robert Hood and Daniel Bell.
+                      </Typography>
+                    </ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText sx={{ marginY: 0 }}>
+                      <Typography>
+                        Such trends saw the demise of the soul-infused techno
+                        that typified the original Detroit sound. Robert Hood
+                        has noted that he and Daniel Bell both realized
+                        something was missing from techno in the post-rave era.
+                      </Typography>
+                    </ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemText sx={{ marginY: 0 }}>
+                      <Typography>
+                        Design is a plan or specification for the construction
+                        of an object or system or for the implementation of an
+                        activity or process, or the result of that plan or
+                        specification in the form of a prototype, product or
+                        process. The national agency for design: enabling
+                        Singapore to use design for economic growth and to make
+                        lives better.
+                      </Typography>
+                    </ListItemText>
+                  </ListItem>
+                </List>
+                <Grid container spacing={1}>
+                  {images.slice(0, 4).map((image, index) => (
+                    <Grid item key={index} xs={3}>
+                      <img src={image.src} alt="" />
+                      <Typography>{image.caption}</Typography>
+                    </Grid>
+                  ))}
+                </Grid>
+                <Grid container spacing={1} style={{ margin: " 2em 0 " }}>
+                  {images.slice(4, 8).map((image, index) => (
+                    <Grid item key={index} xs={3}>
+                      <img src={image.src} alt="" />
+                      <Typography>{image.caption}</Typography>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Item>
+            </Grid>
+            <Grid xs={6}>
+              <Grid>
+                <Booking>
+                  <Typography>Start Booking</Typography>
+                  <Typography>$280</Typography>
+                  <Typography>per night</Typography>
+                  <Typography variant="caption">Discount 20% Off </Typography>
+                  <BookingCalender />
+                </Booking>
               </Grid>
             </Grid>
-          </Box>
-        </ThemeProvider>
-      </Container>
+
+            <Grid xs={6}>
+              <Item>4</Item>
+            </Grid>
+          </Grid>
+        </Box>
+      </ThemeProvider>
+      {/* </Container> */}
     </>
   );
 };
