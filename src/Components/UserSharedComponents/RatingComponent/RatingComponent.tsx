c1a62c67789a23b427006a5c5ba04dfbe77767a1
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2024-01-24 12:06:11 +0200

    add rating comp

---
+++ src/Components/UserSharedComponents/RatingComponent/RatingComponent.tsx
@@ -0,0 +1,27 @@
+/** @format */
+
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
+
+export default function RatingComponent() {
+  const [value, setValue] = React.useState<number | null>(2);
+
+  return (
+    <Box
+      sx={{
+        "& > legend": { mt: 2 },
+      }}
+    >
+      <Typography component="legend">Rate</Typography>
+      <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+    </Box>
+  );
+}
