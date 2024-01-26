6eb3cc9626b8f3363d65ecd9474570e61281abb7
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2024-01-26 22:13:08 +0200

    add feadback component

--- src/Components/UserSharedComponents/Feedback/FeedbackComponent.tsx
+++ src/Components/UserSharedComponents/Feedback/FeedbackComponent.tsx
@@ -1,64 +1,53 @@
 /** @format */
 
-import FormControl from "@mui/joy/FormControl";
-import FormLabel from "@mui/joy/FormLabel";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
 import * as React from "react";
-import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import IconButton from "@mui/joy/IconButton";
-import Textarea from "@mui/joy/Textarea";
-import Typography from "@mui/joy/Typography";
-const FeedbackComponent = () => {
-  const [text, setText] = React.useState("");
-  const addEmoji = (emoji: string) => () => setText(`${text}${emoji}`);
 
+const FeedbackComponent = ({ btnText }) => {
+  const [text, setText] = React.useState("");
+  // const [customerComments, setCustomerComments] = useState('');
+
+  const handleCommentsChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    // Handle the submission of customer comments, e.g., send to a server, etc.
+    console.log("Customer Comments:", text);
+    // You can perform additional actions here, such as sending the comments to a server.
+  };
   return (
-    <>
-      <FormControl>
-        <FormLabel>Your comment</FormLabel>
-        <Textarea
-          placeholder="Type in here…"
-          value={text}
-          onChange={(event) => setText(event.target.value)}
-          minRows={2}
-          maxRows={4}
-          startDecorator={
-            <Box sx={{ display: "flex", gap: 0.5, flex: 1 }}>
-              <IconButton
-                variant="outlined"
-                color="neutral"
-                onClick={addEmoji("👍")}
-              >
-                👍
-              </IconButton>
-              <IconButton
-                variant="outlined"
-                color="neutral"
-                onClick={addEmoji("🏖")}
-              >
-                🏖
-              </IconButton>
-              <IconButton
-                variant="outlined"
-                color="neutral"
-                onClick={addEmoji("😍")}
-              >
-                😍
-              </IconButton>
-              <Button variant="outlined" color="neutral" sx={{ ml: "auto" }}>
-                See all
-              </Button>
-            </Box>
-          }
-          endDecorator={
-            <Typography level="body-xs" sx={{ ml: "auto" }}>
-              {text.length} character(s)
-            </Typography>
-          }
-          sx={{ minWidth: 300 }}
-        />
-      </FormControl>
-    </>
+    <div>
+      <FormLabel>{btnText === "Rate" ? "Message" : ""}</FormLabel>
+      <TextField
+        // style={{ position: "fixed", left: 0, bottom: 0, width: "100%" }}
+        placeholder="Type in here…"
+        multiline
+        rows={4}
+        variant="outlined"
+        fullWidth
+        value={text}
+        onChange={handleCommentsChange}
+      />
+      <Box style={{ marginTop: "1rem" }}>
+        <Button
+          variant="contained"
+          style={{ textAlign: "end" }}
+          color="primary"
+          onClick={handleSubmit}
+        >
+          {btnText}
+        </Button>
+      </Box>
+    </div>
   );
 };
 
