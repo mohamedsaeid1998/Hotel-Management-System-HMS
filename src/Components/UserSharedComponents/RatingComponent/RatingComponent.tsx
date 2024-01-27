813a088477d14f627d8996e97e2a86dd230dcd77
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2024-01-27 23:45:10 +0200

    add rating component

--- src/Components/UserSharedComponents/RatingComponent/RatingComponent.tsx
+++ src/Components/UserSharedComponents/RatingComponent/RatingComponent.tsx
@@ -4,24 +4,77 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
+import { Controller, useForm } from "react-hook-form";
+import { Button, CircularProgress, TextField } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { setRatingRooms } from "@/Redux/UserPort/RatingSlice/RatingSlice";
 
-export default function RatingComponent() {
-  const [value, setValue] = React.useState<number | null>(2);
+export default function RatingComponent({ roomID }) {
+  const [rateValue, setRateValue] = React.useState<number | null>(2);
+  const [loadingBtn, setLoadingBtn] = React.useState(false);
 
+  const id = roomID;
+  const dispatch = useDispatch();
+  const { register, handleSubmit, control } = useForm({
+    defaultValues: {
+      roomId: id,
+    },
+  });
+
+  const submitData = (data) => {
+    setRating(data);
+  };
+  const setRating = async (data) => {
+    setLoadingBtn(true);
+    try {
+      const roomData = await dispatch(setRatingRooms(data));
+    } finally {
+      setLoadingBtn(false);
+    }
+  };
   return (
     <Box
       sx={{
         "& > legend": { mt: 2 },
       }}
+      component="form"
+      onSubmit={handleSubmit(submitData)}
     >
-      <Typography component="legend">Rate</Typography>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
+      <Controller
+        name="roomId"
+        control={control}
+        render={({ field }) => <input type="hidden" {...field} />}
       />
+      {/* <input type="hidden" {...register("roomId", { value: id })} /> */}
+      <Typography component="legend">Rate</Typography>
+      <Controller
+        name="rating"
+        control={control}
+        defaultValue={rateValue}
+        render={({ field: { onChange } }) => (
+          <Rating name="rating" onChange={onChange} />
+        )}
+      ></Controller>
+
+      <TextField
+        placeholder="Type in here…"
+        multiline
+        rows={4}
+        variant="outlined"
+        fullWidth
+        {...register("review")}
+      />
+
+      <Box style={{ marginTop: "1rem" }}>
+        <Button
+          color="primary"
+          variant="contained"
+          style={{ textAlign: "end" }}
+          type="submit"
+        >
+          {loadingBtn ? <CircularProgress size={24} color="inherit" /> : "Send"}
+        </Button>
+      </Box>
     </Box>
   );
 }
