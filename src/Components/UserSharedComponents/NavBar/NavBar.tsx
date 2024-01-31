9c7bf03a310b58f5758eb2b841a4c1344b8c6d8d
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2024-01-31 02:25:56 +0200

    add burger icon to nave in small screen

--- src/Components/UserSharedComponents/NavBar/NavBar.tsx
+++ src/Components/UserSharedComponents/NavBar/NavBar.tsx
@@ -1,15 +1,21 @@
+/** @format */
+
 import {
   AppBar,
   Badge,
   Box,
   Button,
+  Drawer,
   IconButton,
+  List,
+  ListItem,
   Menu,
   MenuItem,
   Stack,
   Toolbar,
   Tooltip,
   Typography,
+  useMediaQuery,
 } from "@mui/material";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./NavBar.module.scss";
@@ -19,7 +25,8 @@ import { defaultImage } from "@/Assets/Images";
 import { useDispatch, useSelector } from "react-redux";
 import { UserDetails } from "@/Redux/Features/Admin/Users/GetUserDetailsSlice";
 import { getFavoriteItems } from "@/Redux/Features/Portal/Favorites/GetAllFavoritesSlice";
-
+import MenuIcon from "@mui/icons-material/Menu";
+import style from "./NavBar.module.scss";
 const NavBar = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
@@ -39,8 +46,8 @@ const NavBar = () => {
     const element = await dispatch(UserDetails(userId));
     // @ts-ignore
     setUserData(element.payload.data.user);
-  }
-const [favoriteItemsCount, setFavoriteItemsCount] = useState(0)
+  };
+  const [favoriteItemsCount, setFavoriteItemsCount] = useState(0);
   const getFavoriteData = useCallback(async () => {
     try {
       // @ts-ignore
@@ -78,126 +85,210 @@ const [favoriteItemsCount, setFavoriteItemsCount] = useState(0)
   const handleClose = () => {
     setAnchorEl(null);
   };
+  const isSmallScreen = useMediaQuery("(max-width:600px)");
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <>
       <AppBar color="inherit" className="nav">
         <Toolbar>
+          {isSmallScreen ? (
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={() => setDrawerOpen(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+          ) : (
+            ""
+          )}
+
           <Typography
-            className="subNav"
+            className={`subNav ${isSmallScreen ? "centerText" : ""}`}
             variant="h4"
             component="div"
             color="initial"
           >
             {" "}
-            <Typography variant="" className="blueColor">
+            <Typography
+              variant=""
+              className="blueColor"
+              style={{ fontSize: "clamp(2rem, 5vw, 3rem)" }}
+            >
               Stay
             </Typography>
             cation.
           </Typography>
-          <Stack className="navList" direction="row">
-            <Link
-              className={`navLink ${pathname === "/" ? "activeLink" : ""}`}
-              to={"./"}
-            >
-              Home
-            </Link>
-            <Link
-              className={`navLink ${
-                pathname?.includes("explore") ? "activeLink" : ""
-              }`}
-              to={"./explore"}
-            >
-              Explore
-            </Link>
-            <Link
-              className={`navLink ${
-                pathname === "/room-reviews" ? "activeLink" : ""
-              }`}
-              to={"./room-reviews"}
-            >
-              Reviews
-            </Link>
-            <IconButton aria-label={notificationsLabel(100)}>
-              <Badge
-                badgeContent={
-                  favoriteItemsCount === 0 ? "0" : favoriteItemsCount
-                }
-                color="primary"
+          {isSmallScreen ? (
+            ""
+          ) : (
+            <Stack className="navList" direction="row">
+              <Link
+                className={`navLink ${pathname === "/" ? "activeLink" : ""}`}
+                to={"./"}
               >
-                <Favorite onClick={() => navigate("./favorite-rooms")} />
-              </Badge>
-            </IconButton>
-            {!localStorage.getItem("authToken") ? (
-              <>
+                Home
+              </Link>
+              <Link
+                className={`navLink ${
+                  pathname?.includes("explore") ? "activeLink" : ""
+                }`}
+                to={"./explore"}
+              >
+                Explore
+              </Link>
+              <Link
+                className={`navLink ${
+                  pathname === "/room-reviews" ? "activeLink" : ""
+                }`}
+                to={"./room-reviews"}
+              >
+                Reviews
+              </Link>
+              <IconButton aria-label={notificationsLabel(100)}>
+                <Badge
+                  badgeContent={
+                    favoriteItemsCount === 0 ? "0" : favoriteItemsCount
+                  }
+                  color="primary"
+                >
+                  <Favorite onClick={() => navigate("./favorite-rooms")} />
+                </Badge>
+              </IconButton>
+              {!localStorage.getItem("authToken") ? (
+                <>
+                  <Button
+                    className="navBtn"
+                    variant="contained"
+                    onClick={() => navigate("./login")}
+                  >
+                    Login Now
+                  </Button>
+                  <Button
+                    className="navBtnSign"
+                    variant="contained"
+                    onClick={() => navigate("./register")}
+                  >
+                    Sign Up
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Box className="navAvatar">
+                    <Tooltip title="Open settings">
+                      <IconButton>
+                        <img
+                          className="nav-img"
+                          src={
+                            userData?.profileImage == null
+                              ? defaultImage
+                              : `http://res.cloudinary.com/dpa4yqvdv/image/upload/v1705784141/users/fod9w8mryr5c5raufxwc.jpg`
+                          }
+                          id="demo-positioned-button"
+                          aria-controls={
+                            open ? "demo-positioned-menu" : undefined
+                          }
+                          aria-haspopup="true"
+                          aria-expanded={open ? "true" : undefined}
+                          onClick={handleClick}
+                        />
+                      </IconButton>
+                    </Tooltip>
+                  </Box>
+                  <Menu
+                    id="demo-positioned-menu"
+                    aria-labelledby="demo-positioned-button"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      <Typography
+                        variant="caption"
+                        color="red"
+                        onClick={() => handelLogout()}
+                      >
+                        Log Out
+                      </Typography>
+                    </MenuItem>
+                  </Menu>
+                  <Box className="navInfo">
+                    <Typography variant="caption" color="initial">
+                      {userData?.userName}
+                    </Typography>
+                    {/* <Typography variant="caption" color="initial">
+                    {userData?.email}
+                  </Typography> */}
+                  </Box>
+                </>
+              )}
+            </Stack>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="top"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <List className={style.navStyle}>
+          <ListItem>
+            <Link to="/">Home</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/explore">Explore</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/room-reviews">Reviews</Link>
+          </ListItem>
+          <ListItem>
+            <Link to="/favorite-rooms">Favorite Rooms</Link>
+          </ListItem>
+          {!localStorage.getItem("authToken") ? (
+            <>
+              <ListItem>
                 <Button
                   className="navBtn"
                   variant="contained"
-                  onClick={() => navigate("./login")}
+                  onClick={() => {
+                    setDrawerOpen(false);
+                    navigate("/login");
+                  }}
                 >
                   Login Now
                 </Button>
+              </ListItem>
+              <ListItem>
                 <Button
                   className="navBtnSign"
                   variant="contained"
-                  onClick={() => navigate("./register")}
+                  onClick={() => {
+                    setDrawerOpen(false);
+                    navigate("/register");
+                  }}
                 >
                   Sign Up
                 </Button>
-              </>
-            ) : (
-              <>
-                <Box className="navAvatar">
-                  <Tooltip title="Open settings">
-                    <IconButton>
-                      <img
-                        className="nav-img"
-                        src={
-                          userData?.profileImage == null
-                            ? defaultImage
-                            : `http://res.cloudinary.com/dpa4yqvdv/image/upload/v1705784141/users/fod9w8mryr5c5raufxwc.jpg`
-                        }
-                        id="demo-positioned-button"
-                        aria-controls={
-                          open ? "demo-positioned-menu" : undefined
-                        }
-                        aria-haspopup="true"
-                        aria-expanded={open ? "true" : undefined}
-                        onClick={handleClick}
-                      />
-                    </IconButton>
-                  </Tooltip>
-                </Box>
-                <Menu
-                  id="demo-positioned-menu"
-                  aria-labelledby="demo-positioned-button"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
+              </ListItem>
+            </>
+          ) : (
+            <>
+              <ListItem>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    setDrawerOpen(false);
+                    navigate("/logout");
+                  }}
                 >
-                  <MenuItem onClick={handleClose}>
-                    <Typography
-                      variant="caption"
-                      color="red"
-                      onClick={() => handelLogout()}
-                    >
-                      Log Out
-                    </Typography>
-                  </MenuItem>
-                </Menu>
-                <Box className="navInfo">
-                  <Typography variant="caption" color="initial">
-                    {userData?.userName}
-                  </Typography>
-                  <Typography variant="caption" color="initial">
-                    {userData?.email}
-                  </Typography>
-                </Box>
-              </>
-            )}
-          </Stack>
-        </Toolbar>
-      </AppBar>
+                  Logout
+                </Button>
+              </ListItem>
+            </>
+          )}
+        </List>
+      </Drawer>
     </>
   );
 };
