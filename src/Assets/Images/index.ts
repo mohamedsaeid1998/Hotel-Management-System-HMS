217ea3fb573b63e96fa9e40ebb5df63ba6e44db7
mohamedsaeid1998 <105775732+mohamedsaeid1998@users.noreply.github.com>
2024-02-07 00:37:08 +0200

    Charts

--- src/Assets/Images/index.ts
+++ src/Assets/Images/index.ts
@@ -24,6 +24,7 @@ import ocean from "./ocean.jpg";
 import creditCard from "./creditCard.png";
 import paymentDone from "./paymentDone.png";
 import familyReview from "./familyReview.png";
+
 export {
   familyReview,
   bathroom,
